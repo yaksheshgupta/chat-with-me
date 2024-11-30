66dfc4b5869111225640a69132b443ff0b585d75
Yakshesh Gupta <yakshesh.gupta@credain.com>
2024-12-01 02:46:05 +0530

    minor

--- frntend/chat.js
+++ frntend/chat.js
@@ -23,7 +23,8 @@ export function joinChat() {
         return;
     }
 
-    ws = new WebSocket('wss://20.197.1.196:9952');
+    // Example for using the Ngrok public URL for WebSocket connection
+    ws = new WebSocket('wss://d944-2401-4900-6349-6e86-cc9e-f744-d128-6232.ngrok-free.app');
 
     ws.onopen = () => {
         ws.send(JSON.stringify({ type: 'setName', data: username }));
